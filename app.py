410c2adfe2c36423bab96251c40b0a8461fd347c
Alejandro Barrera <114511645+Alejandro9233@users.noreply.github.com>
2023-11-21 22:23:15 +0000

    all tables ready

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from db import load_hectares_from_db, load_weather, load_users_from_db, load_vineyards_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    users = load_users_from_db()
+    vineyards = load_vineyards_from_db()
+    hectare = load_hectares_from_db()
+    temperature = load_weather()
+    return render_template('index.html', users=users, vineyards=vineyards, hectare = hectare, temperature=temperature)
 
 if __name__ == '__main__':
     app.run(debug = True)
--- db.py
+++ db.py
@@ -1,42 +1,95 @@
-from dotenv import load_dotenv
-
-# Load environment variables from the .env file
-load_dotenv()
 import os
-import MySQLdb
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import datetime as dt
+import pytz
+import pandas as pd
+import meteomatics.api as api
 
-# Connect to the database
-connection = MySQLdb.connect(
-  host=os.getenv("DATABASE_HOST"),
-  user=os.getenv("DATABASE_USERNAME"),
-  passwd=os.getenv("DATABASE_PASSWORD"),
-  db=os.getenv("DATABASE"),
-  autocommit=True,
-  ssl_mode="VERIFY_IDENTITY",
-  # See https://planetscale.com/docs/concepts/secure-connections#ca-root-configuration
-  # to determine the path to your operating systems certificate file.
-  ssl={ "ca": "" }
+
+load_dotenv()
+
+host=os.getenv("DATABASE_HOST")
+user=os.getenv("DATABASE_USERNAME")
+passwd=os.getenv("DATABASE_PASSWORD")
+db=os.getenv("DATABASE")
+db_key = f"mysql+pymysql://{user}:{passwd}@{host}/{db}?charset=utf8mb4"
+
+engine = create_engine(
+    db_key,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    }
 )
 
-try:
-    # Create a cursor to interact with the database
-    cursor = connection.cursor()
 
-    # Execute "SHOW TABLES" query
-    cursor.execute("SHOW TABLES")
+def load_hectares_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from Hectare"))
+        result_all = result.fetchall()
+        projects = []
+        for row in result_all:
+            my_dict = {
+                'id': row[0],
+                'temperature': row[1],
+                'sprinkler': row[2],
+                'light': row[3],
+                'ceiling': row[4],
+                'timeStamp': row[5],
+            }
+            projects.append(my_dict)
+        return projects
 
-    # Fetch all the rows
-    tables = cursor.fetchall()
+def load_users_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from Usuarios"))
+        result_all = result.fetchall()
+        projects = []
+        for row in result_all:
+            my_dict = {
+                'id': row[0],
+                'name': row[1].upper(),
+                'vineyards': row[2],
+            }
+            projects.append(my_dict)
+        return projects
+    
 
-    # Print out the tables
-    print("Tables in the database:")
-    for table in tables:
-        print(table[0])
+def load_vineyards_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from vineyards"))
+        result_all = result.fetchall()
+        projects = []
+        for row in result_all:
+            my_dict = {
+                'id': row[0],
+                'name': row[1].upper(),
+                'imgURL': row[2],
+                'hectares': row[3],
+                'location': row[4],
+            }
+            projects.append(my_dict)
+        return projects
+print(load_vineyards_from_db())
 
-except MySQLdb.Error as e:
-    print("MySQL Error:", e)
+username = os.getenv("METEO_USER")
+password = os.getenv("METEO_PASSWORD")
 
-finally:
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+def load_weather():
+    #Parameters
+    coordinates = [(25.68, -100.31)]
+    parameters = ['t_2m:C']
+    model = 'mix'
+    timezone = pytz.timezone('America/Monterrey')
+    startdate = dt.datetime.now().astimezone(timezone)
+    enddate = startdate + dt.timedelta(hours=1)
+    interval = dt.timedelta(hours=1)
+    #Request
+    df = api.query_time_series(coordinates, startdate, enddate, interval, parameters, username, password, model=model)
+    #Formating
+    df.index = pd.DatetimeIndex(df.index.get_level_values('validdate'))
+    df.index = df.index.tz_convert(timezone)
+    data_array = df.to_numpy()
+    return data_array[0][0]
